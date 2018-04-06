b0c1c46566f06b3bdfb3a78224d0b59586942823
SchruteFarms23 <romanmullady@gmail.com>
2018-04-06 19:06:12 -0400

    Made widget container component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import WidgetContainer from './components/containers/WidgetContainer';
+
 
 class App extends Component {
   render() {
@@ -10,12 +13,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WidgetContainer />
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
