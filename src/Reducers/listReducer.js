1fc748ff1f456f8214e9d0b4f9f59370956a4b14
SchruteFarms23 <romanmullady@gmail.com>
2018-04-07 17:51:08 -0400

    added styling to messages

--- src/Reducers/listReducer.js
+++ src/Reducers/listReducer.js
@@ -1,4 +1,4 @@
-export default function listReducer(state={chatList:[{type:"bot", message:"Hi, how can I help you today?"},{type:"user", message:"I want to know my bill statement"}]},action){
+export default function listReducer(state={chatList:[{type:"bot", message:"Hi, how can I help you today dfdsafhsalfkhdksflhdsaljkfhdshfdsf dsflkjhdsfhdsfhlsakfh lhkjdsfhdsfkljadsfhdslkf hfdshjfdsfhkjashkjf fsfdfd fgdfgfdg gfdgfdgds gggs?"},{type:"user", message:"I want to know my bill statement d fsdf fsdfasf fsdfdsaf fdsfsdf fdsfas fsdf assdf sadfdf fdsfadsfas fsf ds fsfsafdfds fdsfsdafs dsdfsffaa sfsafsfd safsdfsadf sfsdfasfds fsdaf safsd fsasfdsds"}]},action){
   switch(action.type){
     default:
       return state;
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,36 @@
 import React from 'react'
+const userMessage = {
+      color: 'white',
+      padding: '18px 20px',
+      lineHeight: '25px',
+      fontSize: '16px',
+      borderRadius: '20px',
+      marginBottom: '30px',
+      width: '60%',
+      position: 'relative',
+      background: '#87CEFA',
+      float: 'right'
+}
+const botMessage = {
+      color: 'white',
+      padding: '18px 20px',
+      lineHeight: '25px',
+      fontSize: '16px',
+      borderRadius: '20px',
+      marginBottom: '30px',
+      width: '60%',
+      position: 'relative',
+      background: '#83E32F',
+      float:'left'
+}
+
+
 
 const Message = ({content}) =>{
   return(
-    <p>{content.message}</p>
+      <div style={content.type === "bot" ? botMessage : userMessage} >
+        {content.message}
+      </div>
   )
 }
 
