fc0d3bcfbc2d6fc632272d56e592b2ee96ef0cb6
SchruteFarms23 <romanmullady@gmail.com>
2018-04-10 00:28:13 -0400

    added <a> tag to make yelp conent clickable

--- src/components/YelpResult.js
+++ src/components/YelpResult.js
@@ -3,12 +3,14 @@ import React from 'react'
 const YelpResult = ({details}) =>{
   console.log(details)
   return(
-    <div className="card">
+  <a href={details.url} target="_blank">
+   <div className="card">
       <img src={details.image_url} alt="Avatar" style={{width:"80%"}}/>
         <div className="container">
           <h4><b>{details.name}</b></h4>
         </div>
     </div>
+  </a>
   )
 }
 
